dd3423fbdda069339ac2336059112cc11d5d12b6
Vladimir <grandtwix33@mail.ru>
2022-10-31 18:30:54 +0500

    add Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает положительное число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write(" Введите положительное число: ");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+while(count <= N)
+{
+    if(count % 2 == 0)
+    {
+        Console.Write($"{count}, ");
+        count++;
+    }
+count++;
+}
+
+
+
+
