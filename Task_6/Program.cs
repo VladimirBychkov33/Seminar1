6c021e91dd93f68961b9652ca3cade1a7f3b5eb7
Vladimir <grandtwix33@mail.ru>
2022-10-31 15:29:21 +0500

    add numbers  in Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+Console.Write(" Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if(number%2 == 0)
+{
+    Console.WriteLine("Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число НЕ является чётным");
+}
